78eeb01bbe3fe6afad25d8d9bbe5f560167f031a
frealexandro <jhoner300@gmail.com>
2024-10-29 21:28:50 -0500

    we fix the index title

--- RandIA/utils.py
+++ RandIA/utils.py
@@ -6,7 +6,7 @@ def lang() -> rx.Component:
 
 
 
-index_title = "RandiA | Escala tu operación analytics y encuentra soluciones de inteligencia artificial para tu organización en un marketplace confiable y sin complicaciones." 
-index_description = "Escala tu operación analytics y encuentra soluciones de inteligencia artificial para tu organización en un marketplace confiable y sin complicaciones."
+index_title = "RandiA | THE BIGGEST AI MARKETPLACE" 
+index_description = "THE BIGGEST AI MARKETPLACE"
 
 preview = "https://randia.reflex.run/randia_preview.png"
