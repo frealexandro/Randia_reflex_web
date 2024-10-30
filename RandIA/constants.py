f096bd816c3be9c964ea96002aa790c393db7a16
frealexandro <jhoner300@gmail.com>
2024-10-29 21:33:42 -0500

    se fix the link of instagram

--- RandIA/constants.py
+++ RandIA/constants.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 #! social media section
 
-INSTAGRAM = "https://www.instagram.com/randia.io?igsh=MTd3aHZqYzV5M3djZA%3D%3D&utm_source=qr"
+INSTAGRAM = "https://www.instagram.com/soy_randia?igsh=cW15MHU1dTlhcHQ4"
 LINKEDIN = "https://www.linkedin.com/company/105248815/admin/page-posts/published/"
