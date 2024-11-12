079947a67481ce5fc0d0657ad19f088e7b7646ef
frealexandro <jhoner300@gmail.com>
2024-11-12 12:21:12 -0500

    FIX ERROR SERVER

--- rxconfig.py
+++ rxconfig.py
@@ -2,8 +2,4 @@ import reflex as rx
 
 config = rx.Config(
     app_name="RandIA",
-    api_url="https://reflex-app-406848975537.us-central1.run.app",
-    deploy_url="https://reflex-app-406848975537.us-central1.run.app",
-    backend_host="0.0.0.0",
-    cors_allowed_origins=["*"]
 )
